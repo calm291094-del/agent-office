23adc2415151e5d6ec0d9c30ffc4e684fc88586b
Kalm <calm291094@gmail.com>
2026-07-10 11:06:06 -0400

    Update main.tsx

--- pixel-agents-repo/webview-ui/src/main.tsx
+++ pixel-agents-repo/webview-ui/src/main.tsx
@@ -1,21 +1,9 @@
-import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-
-import App from './App.tsx';
-import { isBrowserRuntime } from './runtime';
-
-async function main() {
-  if (isBrowserRuntime) {
-    const { initBrowserMock } = await import('./browserMock.js');
-    await initBrowserMock();
-  }
-  createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-      <App />
-    </StrictMode>,
-  );
-}
-
-main().catch(console.error);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
